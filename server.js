b40bba9c2b1653d49bfeaa84af253ec188cb2a84
jolo2745 <jhelyar04@gmail.com>
2026-02-12 19:30:53 +0000

    Initial OAuth backend

---
+++ server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Backend running 🚀");
+});
+
+app.get("/auth/lightspeed/callback", (req, res) => {
+    const code = req.query.code;
+
+    if (!code) {
+        return res.status(400).send("No auth code provided");
+    }
+
+    res.send("Auth code received: " + code);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
