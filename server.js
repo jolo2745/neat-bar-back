e40f11b31dad8e5b877e0af33f749f93734f152c
jolo2745 <jhelyar04@gmail.com>
2026-02-12 20:30:44 +0000

    change of port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
     res.send("Backend running 🚀");
