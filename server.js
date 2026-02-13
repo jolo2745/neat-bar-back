c246d85f9facf5254c9be8c9205e67db61ab2805
jolo2745 <jhelyar04@gmail.com>
2026-02-13 15:12:02 +0000

    looking for rejection error

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+process.on('unhandledRejection', (err) => {
+    console.error("Unhandled Rejection:", err);
+});
+
+process.on('uncaughtException', (err) => {
+    console.error("Uncaught Exception:", err);
+});
+
+
 require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
